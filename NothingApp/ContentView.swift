84d91fae94e334e468c004bf878c044aab389d82
Mohamed Salem <cp.e.salem@gmail.com>
2023-10-16 12:51:21 +0200

    Project commit

---
+++ NothingApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NothingApp
+//
+//  Created by Mohamed Salem on 16.10.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NothingApp/NothingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NothingAppApp.swift
+//  NothingApp
+//
+//  Created by Mohamed Salem on 16.10.23.
+//
+
+import SwiftUI
+
+@main
+struct NothingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
